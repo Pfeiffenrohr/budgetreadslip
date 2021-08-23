3055bddba2aa38ad64503887fbf2db9dbb823331
richard lechner <richard@richardlechner.de>
2021-08-23 13:07:00 +0200

    peerBerry eingeführt

--- transformer/src/main/java/transformer/transform/Transform.java
+++ transformer/src/main/java/transformer/transform/Transform.java
@@ -51,6 +51,11 @@ public class Transform {
         content = tr.parseFileViaInvest(txt);
         tr.writeFile(content,args[1],url,"ViaInvest");
         } 
+        if (args[2].equals("peerberry"))
+        {
+        content = tr.parseFilePeerBerry(txt);
+        tr.writeFile(content,args[1],url,"PeerBerry");
+        } 
       
     }
     
@@ -59,7 +64,7 @@ public class Transform {
         String body = transformToJson(list);
         System.out.println(body);
       
-        if (company.equals("Mintos") ||company.equals("ViaInvest") )
+        if (company.equals("Mintos") ||company.equals("ViaInvest") || company.equals("PeerBerry"))
         {
             url=url + "/p2p";
         }
@@ -302,6 +307,58 @@ public class Transform {
             }
             return content;
         }
+       
+       
+       private String parseFilePeerBerry(String txt)
+       {
+           list = new ArrayList<SlipEntry>();
+           String content=""; 
+           String splited [] = txt.trim().split("\n");
+           System.out.println("File has " + splited.length + " lines");
+           int count = 0;
+           String summe = "";
+           // Suche den Anfang
+           while ( count < splited.length && !splited[count].contains("Invested funds")) {
+               count++;
+               continue;
+           }
+       //  System.out.println(splited[count]);
+           while (count < splited.length && !splited[count].contains("Profit") ) {
+               SlipEntry se = new SlipEntry();
+             /*  while (count < splited.length && !splited[count].startsWith("=AC")) {
+                   System.out.println(splited[count]);
+                   count++;
+                   continue;
+               }*/
+               if (count >= splited.length) return content;
+               //System.out.println(splited[count]);
+               String name = "Gesamtertrag";
+               se.setName(name);
+               count++;
+               if (count >= splited.length) return content;
+               while (count < splited.length && !splited[count].contains("Profit")) {
+                   count++;
+                   if (count >= splited.length) return content;
+                   continue;
+               }
+               summe = splited[count];
+               summe = summe.substring(summe.indexOf(":") + 2);
+             //  summe = summe.substring(0, summe.indexOf(":"));
+               
+               
+               // summe = summe.substring(0, summe.indexOf(" "));
+      
+               System.out.println("Summe =" + summe);
+               content=content +summe+" { \n";
+               se.setSum(summe);
+               count++;
+               list.add(se);
+               //Wir wollen nur eine zeile haben !!
+               return content;
+               //if (count >= splited.length) return content;
+           }
+           return content;
+       }
        
     private String readFile(String inputfile)
     { 
